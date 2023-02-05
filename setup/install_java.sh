c458298683954fc3674ec8a798180e0f4f7cc907
lymperop <jimlibo13@gmail.com>
2023-02-05 20:32:01 +0200

    reorganized the repo, deleted unecessary files

--- setup/install_java.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y openjdk-8-jdk
-java -version
-
-
--- setup/nat_slave.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ENDPOINT_INTERFACE=$(cat /etc/hosts | grep master | awk '{print $1}')
-route add default gw $ENDPOINT_INTERFACE
-echo "Gateway now points to $ENDPOINT_INTERFACE"
